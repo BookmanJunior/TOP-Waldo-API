cb0d521d4671e98b6582cf1d2a06b846a3e706a6
Azam Gulov <gulovazam@gmail.com>
2024-05-25 18:56:39 +0500

    Formatted

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,15 @@
-import express from "express"
-import logger from "morgan"
-import { configDotenv } from "dotenv"
+import express from 'express';
+import logger from 'morgan';
+import { configDotenv } from 'dotenv';
 
-configDotenv()
-const app = express()
+configDotenv();
+const app = express();
 const port = process.env.PORT || 3000;
 
-
-app.use(logger("dev"))
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(port, () => {
-  console.log(`Starting server on port ${port}`)
-})
+  console.log(`Starting server on port ${port}`);
+});
