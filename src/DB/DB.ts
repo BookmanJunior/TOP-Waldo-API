a9b347e8c250110f621858695c26930e6cb675a0
Azam Gulov <gulovazam@gmail.com>
2024-05-26 21:11:47 +0500

    Initial commit

---
+++ src/DB/DB.ts
@@ -0,0 +1,19 @@
+import { configDotenv } from 'dotenv';
+import pg from 'pg';
+const { Pool } = pg;
+
+configDotenv();
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB,
+  application_name: 'Waldo'
+});
+
+export const dbquery = async (text: string, params?: string[]) => {
+  const res = await pool.query(text, params);
+  return res;
+};
