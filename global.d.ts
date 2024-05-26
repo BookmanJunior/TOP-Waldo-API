9ce3ab36fda21bf3d0007e4599f84ef49b301c82
Azam Gulov <gulovazam@gmail.com>
2024-05-26 21:07:03 +0500

    Add Env variable types

---
+++ global.d.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+namespace NodeJS {
+  interface ProcessEnv {
+    DB_USER: string;
+    DB_PASSWORD: string;
+    DB: string;
+    DB_HOST: string;
+    DB_PORT: number;
+  }
+}
