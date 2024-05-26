39529d93fac10ea3ed320eddef1d0174b0c75cba
Azam Gulov <gulovazam@gmail.com>
2024-05-26 21:10:04 +0500

    Add route
    remove dotenv config

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
 import express from 'express';
 import logger from 'morgan';
-import { configDotenv } from 'dotenv';
 
-configDotenv();
+import MapRouter from './Routes/Map.js';
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use('/maps', MapRouter);
 
 app.listen(port, () => {
   console.log(`Starting server on port ${port}`);
