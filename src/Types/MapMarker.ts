8d42921ba94494fb7057db80eb298423c2682fee
Azam Gulov <gulovazam@gmail.com>
2024-05-30 19:10:45 +0500

    Move common properties to a separate interface

--- src/Types/MapMarker.ts
+++ src/Types/MapMarker.ts
@@ -1,14 +1,15 @@
-export interface MapMarkerModel {
-  id: number;
+interface Coordinates {
   x: number;
   y: number;
+}
+
+export interface MapMarkerModel extends Coordinates {
+  id: number;
   name: string;
   img: string;
   map_id: number;
 }
 
-export interface mapMarkersPost {
-  x: number;
-  y: number;
+export interface mapMarkersPost extends Coordinates {
   charName: string;
 }
