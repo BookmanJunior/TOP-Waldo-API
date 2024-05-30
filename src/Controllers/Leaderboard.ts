be71cbdc7cc845158fac9aad2df6817b113605de
Azam Gulov <gulovazam@gmail.com>
2024-05-30 12:34:44 +0500

    Add LIMIT property to db query
    fix leadboard_post db query using wrong property value

--- src/Controllers/Leaderboard.ts
+++ src/Controllers/Leaderboard.ts
@@ -3,7 +3,7 @@ import { dbquery } from '../DB/DB.js';
 
 export const leaderboard_get = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const query = await dbquery('SELECT * FROM LEADERBOARD ORDER BY TIME ASC');
+    const query = await dbquery('SELECT * FROM LEADERBOARD ORDER BY TIME ASC LIMIT 10');
     res.status(200).send(query.rows);
   } catch (error) {
     next(error);
@@ -22,7 +22,7 @@ export const leaderboard_post = async (
   next: NextFunction
 ) => {
   try {
-    const queryParams = [req.body.name, req.body.time, req.body.time];
+    const queryParams = [req.body.name, req.body.time, req.body.map_id];
     await dbquery('INSERT INTO LEADERBOARD (name, time, map_id) VALUES ($1, $2, $3)', queryParams);
     next();
   } catch (error) {
