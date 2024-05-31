512995572e5b4c7650296f146bafd16c419f277f
Azam Gulov <gulovazam@gmail.com>
2024-05-31 16:51:54 +0500

    Add parameter to get route
    remove next middleware call from post route

--- src/Routes/Leaderboard.ts
+++ src/Routes/Leaderboard.ts
@@ -3,8 +3,8 @@ import { leaderboard_get, leaderboard_post } from '../Controllers/Leaderboard.js
 
 const router = Router();
 
-router.get('/', leaderboard_get);
+router.get('/:map_id', leaderboard_get);
 
-router.post('/', leaderboard_post, leaderboard_get);
+router.post('/', leaderboard_post);
 
 export default router;
