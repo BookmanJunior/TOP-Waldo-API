9f527594948acfd274265eb895ee855b3a2e90d6
Azam Gulov <gulovazam@gmail.com>
2024-05-31 16:51:04 +0500

    Add request parameter to leaderboard_get middleware
    add getLeaderboard helper function
    remove next() middleware call from leaderboard_post
    move types to corresponding type file

--- src/Controllers/Leaderboard.ts
+++ src/Controllers/Leaderboard.ts
@@ -1,31 +1,35 @@
 import { NextFunction, Request, Response } from 'express';
 import { dbquery } from '../DB/DB.js';
+import { Leaderboard, leaderboardPostBody, mapId } from '../Types/Leaderboard.js';
 
-export const leaderboard_get = async (req: Request, res: Response, next: NextFunction) => {
+export const leaderboard_get = async (req: Request<mapId>, res: Response, next: NextFunction) => {
   try {
-    const query = await dbquery('SELECT * FROM LEADERBOARD ORDER BY TIME ASC LIMIT 10');
+    const query = await getLeaderboard(req.params.map_id);
     res.status(200).send(query.rows);
   } catch (error) {
     next(error);
   }
 };
 
-interface leaderboardPostBody {
-  name: string;
-  time: number;
-  map_id: number;
-}
-
 export const leaderboard_post = async (
   req: Request<Record<string, never>, Record<string, never>, leaderboardPostBody>,
   res: Response,
   next: NextFunction
 ) => {
+  const queryParams = [req.body.name, req.body.time, req.body.map_id];
+
   try {
-    const queryParams = [req.body.name, req.body.time, req.body.map_id];
     await dbquery('INSERT INTO LEADERBOARD (name, time, map_id) VALUES ($1, $2, $3)', queryParams);
-    next();
+    const query = await getLeaderboard(req.body.map_id);
+
+    res.status(200).send(query.rows);
   } catch (error) {
     next(error);
   }
 };
+
+async function getLeaderboard(map_id: number) {
+  return await dbquery<Leaderboard>(
+    `SELECT * FROM LEADERBOARD WHERE MAP_ID = ${map_id} ORDER BY TIME ASC LIMIT 10`
+  );
+}
