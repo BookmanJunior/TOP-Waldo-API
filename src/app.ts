1f23d993111b481a05d6a3b3fa8714c323cc79e4
Azam Gulov <gulovazam@gmail.com>
2024-06-06 20:54:09 +0500

    Add helmet and rate limit

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import logger from 'morgan';
 import cors from 'cors';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 
 import MapRouter from './Routes/Map.js';
 import LeaderboardRouter from './Routes/Leaderboard.js';
@@ -8,6 +10,12 @@ import MapMakerRouter from './Routes/MapMarker.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: 'draft-7',
+  legacyHeaders: false
+});
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -17,6 +25,8 @@ app.use(
     origin: ['http://localhost:5173', 'https://top-waldo.vercel.app']
   })
 );
+app.use(helmet());
+app.use(limiter);
 
 app.use('/maps', MapRouter);
 app.use('/leaderboard', LeaderboardRouter);
