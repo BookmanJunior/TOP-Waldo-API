3e50c5a1b5d2d1bd706b53f2230baafddc39eab4
Azam Gulov <gulovazam@gmail.com>
2024-06-08 18:46:25 +0500

    Add validation chain to leaderboard_post middlware

--- src/Controllers/Leaderboard.ts
+++ src/Controllers/Leaderboard.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { dbquery } from '../DB/DB.js';
 import { Leaderboard, leaderboardPostBody, mapId } from '../Types/Leaderboard.js';
+import { body, validationResult } from 'express-validator';
 
 export const leaderboard_all_get = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -20,22 +21,41 @@ export const leaderboard_get = async (req: Request<mapId>, res: Response, next: 
   }
 };
 
-export const leaderboard_post = async (
-  req: Request<Record<string, never>, Record<string, never>, leaderboardPostBody>,
-  res: Response,
-  next: NextFunction
-) => {
-  const queryParams = [req.body.name, req.body.map_id];
+export const leaderboard_post = [
+  body('name', 'Name must be longer than 1 character and less than 15 characters')
+    .trim()
+    .isLength({ min: 2, max: 15 })
+    .escape(),
+  body('map_id').trim().escape(),
 
-  try {
-    await dbquery('INSERT INTO LEADERBOARD (name, time, map_id) VALUES ($1, $2, $3)', queryParams);
-    const query = await getLeaderboard(req.body.map_id);
+  async (
+    req: Request<Record<string, never>, Record<string, never>, leaderboardPostBody>,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const errorFormatter = ({ msg }: { msg: string }) => {
+      return msg;
+    };
+    const error = validationResult(req).formatWith(errorFormatter);
+    const queryParams = [req.body.name, req.body.map_id];
 
-    res.status(200).send(query.rows);
-  } catch (error) {
-    next(error);
+    if (!error.isEmpty()) {
+      return res.status(400).send(error.mapped());
+    }
+
+    try {
+      await dbquery(
+        'INSERT INTO LEADERBOARD (name, time, map_id) VALUES ($1, $2, $3)',
+        queryParams
+      );
+      const query = await getLeaderboard(req.body.map_id);
+
+      res.status(200).send(query.rows);
+    } catch (error) {
+      next(error);
+    }
   }
-};
+];
 
 async function getLeaderboard(map_id: number) {
   return await dbquery<Leaderboard>(
