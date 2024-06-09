21ee661d5b061bb3632bd62c5d538298531b44c1
Azam Gulov <gulovazam@gmail.com>
2024-06-09 19:01:20 +0500

    Chain next functions in verify post route

--- src/Routes/MapMarker.ts
+++ src/Routes/MapMarker.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { map_markers_post } from '../Controllers/MapMarker.js';
+import { incrementUserScore, isWinner } from '../Controllers/Game.js';
 
 const router = Router();
 
-router.post('/verify', map_markers_post);
+router.post('/verify', map_markers_post, incrementUserScore, isWinner);
 
 export default router;
