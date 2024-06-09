e0f215600d739e6964b44feb0f7b8ad871025d50
Azam Gulov <gulovazam@gmail.com>
2024-06-09 19:15:50 +0500

    Extend SessionData interface
    refactor NodeJS namespace

--- global.d.ts
+++ global.d.ts
@@ -1,12 +1,25 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-namespace NodeJS {
-  interface ProcessEnv {
-    DB_USER: string;
-    DB_PASSWORD: string;
-    DB: string;
-    DB_HOST: string;
-    DB_PORT: number;
-    NODE_ENV: string;
-    COOKIE_SECRET: string;
+import { Express } from 'express';
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      DB_USER: string;
+      DB_PASSWORD: string;
+      DB: string;
+      DB_HOST: string;
+      DB_PORT: number;
+      NODE_ENV: string;
+      COOKIE_SECRET: string | undefined;
+    }
+  }
+}
+
+declare module 'express-session' {
+  interface SessionData {
+    startTime: number;
+    finalTime: number;
+    numOfMarkers: number;
+    foundMarkers: number;
   }
 }
