0f036cb9a23c922514267f2f5662b8e85621f6fb
Azam Gulov <gulovazam@gmail.com>
2024-06-09 19:04:36 +0500

    Chain next middleware in get route

--- src/Routes/Map.ts
+++ src/Routes/Map.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { map_get, maps_get } from '../Controllers/Map.js';
+import { startGame } from '../Controllers/Game.js';
 
 const router = Router();
 
 router.get('/', maps_get);
 
-router.get('/:map_id', map_get);
+router.get('/:map_id', map_get, startGame);
 
 export default router;
